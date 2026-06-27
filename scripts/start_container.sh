230707e8c2fac32132fb47bbb318c2eac5d19fcc
kazalmunirul <kazalmunirul@gmail.com>
2026-06-27 22:04:14 +1000

    update startcontainer

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,5 +3,5 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull kazalmunirul/cloudnative-oct:latest
 # Run the Docker image as a container
-docker run -d --name pyapp -p 5000:5000 kazalmunieze/cloudnative-oct:latest
+docker run -d --name pyapp -p 5000:5000 kazalmunirul/cloudnative-oct:latest
 
